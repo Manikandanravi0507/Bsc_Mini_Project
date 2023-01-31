93dfdd6eaec0bcc3e1798294321d96ce3362d009
manikandanravi0507 <manikandanravi0507@gmail.com>
2023-01-31 22:19:06 +0530

    File name changed to php continue with forgot pass

---
+++ Attendance Management System/Php Files/forgotpassword.php
@@ -0,0 +1,28 @@
+<html>
+    <head>
+        <link rel="icon" type="image/x-icon" href="../Images/logo.jpg">
+        <title>
+            DRBCCC Hindu College Forgot Password
+        </title>
+    </head>
+    <body>
+        <link rel="stylesheet" href="../Style/style.css"/>
+        <div class="forgotpassword">
+            <form method="post " action="login.html">
+         <h2 align="center">FORGOT PASSWORD</h2>
+        <br><br>
+                  <label for="text_Newpass">
+                    New Password
+                    <br><input type="password" name="txt_box" id="text_Newpass"/>
+                 </label><br><br>
+                <label>
+                    <button name="Reset" id="btn_forgot">Reset</button>
+                </label>
+            </div>
+            <?php
+            
+            ?>
+        </form>
+
+    </body>
+</html>
---
+++ Attendance Management System/Php Files/login.php
@@ -0,0 +1,59 @@
+<html>
+   <head>
+     <link rel="icon" type="image/x-icon" href="../Images/logo.jpg">
+   <title>
+           DRBCCC Hindu College Attendance
+	   </title>
+   </head>
+   <body>
+    <link rel="stylesheet" href="../Style/style.css"/>
+    <form method="post" action="login.php">
+      <div class="login">
+        <h2 align="center">STAFF LOGIN</h2>
+        <label for="text_box_u">
+        Username 
+        <br><input type="textbox" name="txt_username" id="text_box_u"/>
+        </label><br><br>
+        <label for="text_box_p">
+          Password
+          <br> <input type="password" name="txt_password" id="text_box_p"/>
+        </label><br><br>
+        <label for="chk_box">
+          <input type="checkbox" name="check_box" id="chk_box"/>
+          Remember Me
+        </label>
+        <br><br>
+        <label><a href="forgotpassword.html">
+          Forgot Password
+        </label>
+        <br><br>
+		<label>
+		  <button id="btn_login" name="login">Login</button>
+		</label>
+      </div>
+    
+<?php
+Session_start();
+$conxn=mysqli_connect("localhost","root","","drbccchc");
+if(isset($_POST['login'])){
+    $username=$_POST['txt_username'];
+    $password=$_POST['txt_password'];
+    $qry="select * from login where username='$_POST[txt_username]' && password='$_POST[txt_password]'";
+    $res=mysqli_query($conxn,$qry);
+    $result=mysqli_num_rows($res);
+    if($result>0)
+    {
+        echo"<script>alert('Login Successfully')</script>";
+        $_Session['txt_username']=$username;
+        header('location:harish.php');
+    }
+    else{
+        echo"<script>alert('Login Failed')</script>";
+        $_Session['txt_username']=$username;
+        header('location:login.php');
+    }
+}
+?>
+</form>
+<body>
+</html>
