86376dedeb3f2a363f0ca428bd06e485401534ae
manikandanravi0507 <manikandanravi0507@gmail.com>
2023-02-02 08:37:04 +0530

    Image is Uploaded

--- Attendance Management System/Php Files/Home.php
+++ Attendance Management System/Php Files/Home.php
@@ -1,5 +1,6 @@
 <html>
     <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
         <link rel="icon" type="image/x-icon" href="../Images/logo.jpg"/>
         <title>
             DRBCCC Hindu College Home
@@ -8,13 +9,15 @@
     <body>
         <link rel="stylesheet" href="../Style/stylehome.css"/>
         <div class="home">
-            <img src="../Images/logo.jpg" />
-            Dharmamurthi Rao Bahadur Calavala Cunnan Chetty's
-                          <br>Hindu College
-        </div>
+            <img src="../Images/logo.jpg"  width = "80" height = "80" align="center"/>
+            Dharmamurthi Rao Bahadur Calavala Cunnan Chetty's Hindu College
+            <br>
+         </div>
+         <div class="container">
 
-        <div class="Container">
-
+         </div>
+         <div class="imagesslide">
+            <img src="../Images/DMB.jpg" width="75%" height=75%"/>
         </div>
     </body>
-</html>
+</html> 
--- Attendance Management System/Php Files/login.php
+++ Attendance Management System/Php Files/login.php
@@ -1,5 +1,6 @@
 <html>
    <head>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
      <link rel="icon" type="image/x-icon" href="../Images/logo.jpg">
    <title>
            DRBCCC Hindu College Attendance
